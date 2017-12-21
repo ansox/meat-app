e43be5b18fbd10287da896c041f2916246e31404
Anderson Floriano <andersonfloriano@MacBook-Pro-de-Anderson.local>
2017-12-21 10:54:12 -0200

    Implemented ControlValueAcessor in radio component

---
+++ src/app/shared/radio/radio.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+@Component({
+  selector: 'mt-radio',
+  templateUrl: './radio.component.html',
+  styleUrls: ['./radio.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RadioComponent),
+      multi: true
+    }
+  ]
+})
+export class RadioComponent implements OnInit, ControlValueAccessor {
+  onChange: any;
+ 
+  value: any;
+
+  @Input() options: RadioOption[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  setValue(value: any) {
+    this.value = value;
+    this.onChange(value);
+  }
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    // throw new Error("Method not implemented.");
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    // throw new Error("Method not implemented.");
+  }
+
+}
